6636dcace50d611f943d3442ddfbcf5dc923e8f1
Ali Chaaban <alivideogamer@hotmail.com>
2017-05-31 19:38:04 +0300

    Add ad to android

---
+++ FoodTruckApp/FoodTruckApp.Android/AdMobRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+namespace FoodTruckApp.Droid
+{
+    public class AdMobRenderer : ViewRenderer<AdMobView, Android.Gms.Ads.AdView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<AdMobView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var ad = new Android.Gms.Ads.AdView(Forms.Context);
+                ad.AdSize = Android.Gms.Ads.AdSize.Banner;
+                ad.AdUnitId = "_your_admob_ad_unit_id_goes_here_";
+
+                var requestbuilder = new Android.Gms.Ads.AdRequest.Builder();
+                ad.LoadAd(requestbuilder.Build());
+
+                SetNativeControl(ad);
+            }
+        }
+    }
+}
